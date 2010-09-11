e326250819ece52af7bea839af00db70bc58b344
Justin Otherguy <justin@justinotherguy.org>
2010-09-11 21:39:34 +0200

    removed buttons and code that belongs to it

--- frontend/javascript/smartmeter.js
+++ frontend/javascript/smartmeter.js
@@ -36,25 +36,7 @@ function getEmptyGroupArray() {
 			
 			var iteratorDate = new Date(iterator);
 			//$('#debug').append('#'+i+':'+iteratorDate+'<br>');
-			switch(f.grouping.value) {
-				case 'year':
-					iteratorDate.setYear(iteratorDate.getYear()+1);
-					break;
-				case 'month':
-					iteratorDate.setMonth(iteratorDate.getMonth()+1);
-					break;
-				case 'day':
-					iteratorDate.setDate(iteratorDate.getDate()+1);
-					break;
-				case 'hour':
-					iteratorDate.setHours(iteratorDate.getHours()+1);
-					break;
-				case 'minute':
-					iteratorDate.setMinutes(iteratorDate.getMinutes()+1);
-					break;
-				default:
-					return empty_array;
-			}
+			iteratorDate.setDate(iteratorDate.getDate()+1);
 			// very bad bug: infinity loop for summer winter change
 			if(i==750) return empty_array;
 			
@@ -74,26 +56,12 @@ function calcMyWindowStart() {
 	var hours = myWindowStart.getHours();
 	var minutes = myWindowStart.getMinutes();
 	
-	var windowSize = f.window.value.substring(0,1);
-	var windowInterval = f.window.value.substring(1);
+	//var windowSize = f.window.value.substring(0,1);
+	var windowSize = "1";
+	//var windowInterval = f.window.value.substring(1);
+	var windowInterval = "MONTH"; // we want to display 1 day (for now)
 	
-	switch(windowInterval) {
-		case 'YEAR':
-			myWindowStart.setYear(myWindowStart.getFullYear()-windowSize);
-			break;
-		case 'MONTH':
 			myWindowStart.setMonth(myWindowStart.getMonth()-windowSize);
-			break;
-		case 'DAY':
-			myWindowStart.setDate(myWindowStart.getDate()-windowSize);
-			break;
-		case 'HOUR':
-			myWindowStart.setHours(myWindowStart.getHours()-windowSize);
-			break;
-		case 'MINUTE':
-			myWindowStart.setMinutes(myWindowStart.getMinutes()-windowSize);
-			break;
-	}
 	return myWindowStart.getTime();
 }
 
@@ -108,109 +76,8 @@ function getGroupedTimestamp(timestamp) {
 	var hours = time.getHours();
 	var minutes = time.getMinutes();
 	
-	// hint: its correct without break;
-	switch(f.grouping.value) {
-		case 'year':
-			month = 1;
-		case 'month':
-			day = 1;
-		case 'day':
 			hours = 0;
-		case 'hour':
-			minutes = 0;
-	}
 	
 	return (new Date(year,month,day,hours,minutes)).getTime();
 }
 
-
-function raw2Energy(raw) {
-	
-	var data_grouped_time = getEmptyGroupArray();
-	var data_grouped = new Array();
-	
-	if(f.grouping.value == '')
-		return;
-		
-	if(raw.data.length == 0) {
-		return [[0,0]];
-	}
-	
-	// for each timestamp in json response
-	for(var i=0;i<raw.data.length;i++) {
-		data_grouped_time[raw.data[i][0]] = raw.data[i][1];
-	}
-	t = 1;
-	switch(f.grouping.value) {
-		case 'year':
-			t *= 365;
-		case 'day':
-			t *= 24;
-		case 'hour':
-			t *= 60;
-		case 'minute':
-			t *= 60;
-	}
-	
-	// transform to proper array and energy instead of pulse count
-	for(var timestamp in data_grouped_time) {
-		
-		if(f.info.value == 'energy')
-			data_grouped.push(data_grouped_time[timestamp]/raw.resolution);
-		else {
-			data_grouped.push([timestamp*1, 3600 * 1000 * data_grouped_time[timestamp] / raw.resolution / t ]);
-		}
-	}
-	
-	if(f.info.value == 'power') {
-		for(var i=data_grouped.length-1;i>=0;i--) {
-			if(data_grouped[i][1]==0 && i>0 && i<data_grouped.length-1 && data_grouped[i][0] > raw.data[0][0]*1000)
-				data_grouped[i][1] = data_grouped[i+1][1];
-		}
-	}
-	
-	return data_grouped;
-}
-
-
-function raw2Power(raw,moving_average) {
-	
-	var last_timestamp = 0;
-	var last_power = 0;
-	var power = 0;
-	var data_line = new Array();
-	
-	if(typeof raw == 'undefined' || raw.data.length == 0) {
-		return [[0,0]];
-	}
-	
-	// for each timestamp in json response
-	for(var i=0;i<raw.data.length;i++) {
-		if(last_timestamp>0) {
-			
-			// difference between this and last timestamp
-			difference = raw.data[i][0] - last_timestamp;
-			
-			// power = 3600*1000/difference/resolution*count
-			power = Math.round(3600 * 1000/difference/raw.resolution*raw.data[i][1]);
-			
-			// average with last power value
-			if(moving_average && last_power>0 && Math.abs(last_power-power)<0.25*power)
-				power = (power +  last_power)/2;
-			
-			// additional value for last_power > power*1.25
-			if(last_power > power*1.25 && last_timestamp) {
-				data_line.push([last_timestamp,power]);
-			}
-			
-			// array with timestamp and power
-			data_line.push([raw.data[i][0],power]);
-		}
-		
-		last_timestamp = raw.data[i][0];
-		last_power = power;
-	}
-	
-	// return array with power@timestamps
-	return data_line;
-}
