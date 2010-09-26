39414ab9e2f1acccf919cba2dc98a43ab37fcfa9
Steffen Vogel <info@steffenvogel.de>
2010-09-26 19:57:37 +0200

    next commit for the controller webinterface

--- controller/php/log.php
+++ controller/php/log.php
@@ -2,6 +2,8 @@
 /**
  * PHP implementation of the controller API
  *
+ * This file should be called by cron every 5-10 minutes
+ *
  * @author Steffen Vogel <info@steffenvogel.de>
  * @copyright Copyright (c) 2010, The volkszaehler.org project
  * @package controller
@@ -24,22 +26,10 @@
  * along with volkszaehler.org. If not, see <http://www.gnu.org/licenses/>.
  */
 
-header('Content-type: application/json');
-
-define('FILE', 'channels.csv');
-
-$methodMapping = array(
-	'POST'		=> 'add',
-	'DELETE'	=> 'delete',
-	'GET'		=> 'get'
-);
-
-$channels = array();
-
-$operation = (isset($_GET['operation'])) ? $_GET['operation'] : $methodMapping[$_SERVER['REQUEST_METHOD']];
-
-// create file if it doesnt exist
-touch(FILE);
+if (php_sapi_name() != 'cli') {
+	header('HTTP/1.1 405 Method Not Allowed');
+	die();
+}
 
 // read channels
 $fd = fopen(FILE, 'r') or die('cant open file');
@@ -48,33 +38,16 @@ while (($data = fgetcsv($fd, 100, ';')) !== FALSE) {
 }
 fclose($fd);
 
-if ($operation == 'add') {
-	// TODO check for duplicate uuid
-	// TODO validate input
-		$channels[] = array(
-			'uuid'		=> $_GET['uuid'],
-			'type'		=> $_GET['type'],
-			'port'		=> $_GET['port'],
-			'value'		=> '',
-			'timestamp'	=> ''
-		);
-}
-elseif ($operation == 'delete') {
-	$channels = array_filter($channels, function($channel) {
-		return $channel['uuid'] != $_GET['uuid'];
-	});
+// log data
+foreach ($channels as $channel) {
+	// TODO log data according to type and port
 }
 
 // save channels
-if (in_array($operation, array('delete', 'add'))) {
-	$fd = fopen(FILE, 'w') or die('cant open file');
-	foreach ($channels as $channel) {
-		fputcsv($fd, $channel, ';');
-	}
-	fclose($fd);
+$fd = fopen(FILE, 'w') or die('cant open file');
+foreach ($channels as $channel) {
+	fputcsv($fd, $channel, ';');
 }
-
-// return all channels as JSON
-echo json_encode($channels);
+fclose($fd);
 
 ?>
