58f45030ec9208bc935c24014b34795cca8924fd
Steffen Vogel <info@steffenvogel.de>
2010-12-24 16:35:00 +0100

    updated cli to stable doctrine version

--- backend/bin/doctrine.php
+++ backend/bin/doctrine.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * doctrine cli configuration and bootstrapping
+ * Doctrine cli configuration
  *
  * @author Steffen Vogel <info@steffenvogel.de>
  * @package doctrine
@@ -54,30 +54,6 @@ $helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
 ));
 
 
-$cli = new \Symfony\Component\Console\Application('Doctrine Command Line Interface', Doctrine\ORM\Version::VERSION);
-$cli->setCatchExceptions(true);
-$cli->setHelperSet($helperSet);
-$cli->addCommands(array(
-	/* DBAL Commands disabled due to http://www.doctrine-project.org/jira/browse/DDC-910
-	new \Doctrine\DBAL\Tools\Console\Command\RunSqlCommand(),
-	new \Doctrine\DBAL\Tools\Console\Command\ImportCommand(),*/
-
-	// ORM Commands
-	new \Doctrine\ORM\Tools\Console\Command\ClearCache\MetadataCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\ClearCache\ResultCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\ClearCache\QueryCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\SchemaTool\CreateCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\SchemaTool\UpdateCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\SchemaTool\DropCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\EnsureProductionSettingsCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\ConvertDoctrine1SchemaCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\GenerateRepositoriesCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\GenerateEntitiesCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\GenerateProxiesCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\ConvertMappingCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\RunDqlCommand(),
-	new \Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand()
-));
-$cli->run();
+\Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet);
 
 ?>
