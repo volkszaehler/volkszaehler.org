bca162de354d4509318477cbf87111cda39732c8
Justin Otherguy <justin@justinotherguy.org>
2011-01-03 00:11:19 +0100

    added a derivative of log_onewire in case you have a 1wire sensor connected to your controller board running ethersex

---
+++ misc/controller/bash/log_onewire_ecmd.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# This is a simple bash script to read Dallas 1-Wire sensors
+# connected to a controller board running ethersex and log their 
+# values to the volkszaehler project.
+#
+# call it with a cronjob similiar to this one:
+#
+# */5 * * * *   ~/bin/log1wire_ecmd.sh
+#
+# @copyright Copyright (c) 2010, The volkszaehler.org project
+# @package controller
+# @license http://www.opensource.org/licenses/gpl-license.php GNU Public License
+# @author Justin Otherguy <justin@justinotherguy.org>
+# @author Steffen Vogel <info@steffenvogel.de>
+#
+##
+# This file is part of volkzaehler.org
+#
+# volkzaehler.org is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# volkzaehler.org is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with volkszaehler.org. If not, see <http://www.gnu.org/licenses/>.
+##
+
+# configuration
+#
+# backend url
+URL="http://volkszaehler.org/neu/backend/index.php"
+
+# sensor settings
+#  id of the sensor
+SENSORID=10a5d600020800b7
+#  ip address of the controller board running ethersex
+ESEXIP=192.168.111.56
+#  uuid of the sensor in the volkszaehler database
+UUID=0f2636b4-12cf-c086-50da-348fab30ece6
+
+## 
+# paths to binaries - you should not need to change these
+CURL=/usr/bin/curl
+NC=/bin/nc
+
+
+# ========= do not change anything below this line ==============
+
+echo "1w convert $SENSORID" |$NC $ESEXIP 2701 -q 1 2>/dev/null | grep -qe OK || exit 1
+TEMPERATURE=`echo 1w get $SENSORID | nc $ESEXIP 2701 -q 1 2>/dev/null | sed -e 's/Temperatur: //'`
+
+$CURL --data "" "$URL/data/$UUID.json?value=$TEMPERATURE"
