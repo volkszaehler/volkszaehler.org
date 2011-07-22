989831c7a05fb1febef1321f7cc17a8322bc3ac9
Steffen Vogel <info@steffenvogel.de>
2011-07-22 11:44:24 +0200

    removed php shorttags

--- htdocs/index.php
+++ htdocs/index.php
@@ -27,7 +27,7 @@
 
 ?>
 
-<?= '<?xml version="1.0" encoding="UTF-8" ?>' ?>
+<?xml version="1.0" encoding="UTF-8" ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
@@ -46,4 +46,3 @@
 	</script>
 </body>
 </html>
-
