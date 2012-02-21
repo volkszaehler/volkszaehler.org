03e23e2cecfce8846be87ca4a1b2231dbff530f5
Steffen Vogel <info@steffenvogel.de>
2012-02-21 08:52:22 +0800

    updated install.sh, added release.sh

--- misc/tools/doctrine.php
+++ misc/tools/doctrine.php
@@ -54,4 +54,6 @@ $helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
 	'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
 ));
 
+\Doctrine\ORM\Tools\Console\ConsoleRunner::run($helperSet);
+
 ?>
