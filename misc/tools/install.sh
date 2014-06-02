7deaaf59a9be6ccfb5da71ee6dd6e19338798b35
Jakob Hirsch <jh@plonk.de>
2014-06-02 12:46:23 +0200

    install script: added missing quotes around variables
    this makes the script fail if there is whitespace in a variable

--- misc/tools/install.sh
+++ misc/tools/install.sh
@@ -46,32 +46,32 @@ tmp_dir=$(mktemp -d)
 ###############################
 # functions
 ask() {
-	question=$1
-	default=$2
+	question="$1"
+	default="$2"
 	read -e -p "$question [$default] "
-	REPLY=${REPLY:-$default}
+	REPLY="${REPLY:-$default}"
 }
 
 cleanup() {
-	if [ -e $tmp_dir ]; then
-		rm -r $tmp_dir
+	if [ -e "$tmp_dir" ]; then
+		rm -r "$tmp_dir"
 	fi
 }
 
 get_admin() {
 	test -n "$db_admin_user" && return
 	ask "mysql admin user?" root
-	db_admin_user=$REPLY
-	sed -i -e "s/^\/*\(\$config\['db'\]\['admin'\]\['user'\]\).*/\1 = '$db_admin_user';/" $config
+	db_admin_user="$REPLY"
+	sed -i -e "s/^\/*\(\$config\['db'\]\['admin'\]\['user'\]\).*/\1 = '$db_admin_user';/" "$config"
 	ask "mysql admin password?"
-	db_admin_pass=$REPLY
-	sed -i -e "s/^\/*\(\$config\['db'\]\['admin'\]\['password'\]\).*/\1 = '$db_admin_pass';/" $config
+	db_admin_pass="$REPLY"
+	sed -i -e "s/^\/*\(\$config\['db'\]\['admin'\]\['password'\]\).*/\1 = '$db_admin_pass';/" "$config"
 }
 
 get_db_name() {
 	test -n "$db_name" && return
 	ask "mysql database?" volkszaehler
-	db_name=$REPLY
+	db_name="$REPLY"
 }
 
 ###############################
@@ -86,29 +86,28 @@ echo -n "checking prerequisites:"
 deps=( php mysql awk sed grep wget mktemp mkdir tar )
 
 for binary in "${deps[@]}"; do
-	if [ `which $binary` ] ; then
-		echo -n " $binary"
+	binpath="$(which $binary)"
+	if [ -n "$binpath" ] ; then
+		echo " $binary: $binpath"
 	else
 		echo
-		echo "you need $binary to run this installer"
+		echo " $binary: not found. Please install to use this script."
 		cleanup && exit 1
 	fi
 done
 echo
 
-php_major=`php --version | grep "^PHP" | awk ' { print $2 } ' | cut -b 1 `
-php_minor=`php --version | grep "^PHP" | awk ' { print $2 } ' | cut -b 3 `
+php_version=$(php --version | grep "^PHP" | awk ' { print $2 } ')
+php_major=$(echo "$php_version" | cut -d. -f1)
+php_minor=$(echo "$php_version" | cut -d. -f2)
 
-echo "php version: $php_major.$php_minor"
+echo "php version: $php_major.$php_minor ($php_major.$php_minor)"
 
-if [ "$php_major" -lt "5" ] ; then
+if [ "$php_major" -lt 5 -o \( "$php_major" -eq 5 -a "$php_minor" -lt 3 \) ]; then
 	echo "you need PHP version 5.3+ to run volkszaehler"
 	cleanup && exit 1
-elif [ "$php_major" == "5" ] ; then
-	if [ "$php_minor" -lt "3" ] ; then
-		echo "you need PHP version 5.3+ to run volkszaehler"
-		cleanup && exit 1
-	fi
+else
+	echo "php version ok"
 fi
 
 ###############################
@@ -116,51 +115,52 @@ echo
 echo "volkszaehler setup..."
 
 ask "volkszaehler path?" /var/www/volkszaehler.org
-vz_dir=$REPLY
+vz_dir="$REPLY"
 
 if [ -e "$vz_dir" ]; then
 	ask "$vz_dir already exists. overwrite?" n
 else
-	mkdir $vz_dir
+	mkdir "$vz_dir"
 	REPLY=y
 fi
 
 if [ "$REPLY" == 'y' ]; then
 	echo "installing volkszaehler.org into $vz_dir"
-	wget -O - $vz_tar | tar xz -C $tmp_dir
-	cp -r $tmp_dir/volkszaehler*/* $vz_dir/
+	wget -O - "$vz_tar" | tar xz -C "$tmp_dir"
+	cp -r "$tmp_dir"/volkszaehler*/* "$vz_dir/"
 fi
 
 ###############################
 echo
-echo "install composer..."
+echo "checking composer..."
 
-if [ `which composer` ]; then
-    COMPOSER=$(which composer)
+for f in composer composer.phar; do
+	COMPOSER=$(which $f) 2>/dev/null
+	test -n "$COMPOSER"  && break
+done
+if [ -n "$COMPOSER" ]; then
+	echo "composer: $COMPOSER"
 else
-    if [ `which composer.phar` ]; then
-        COMPOSER=$(which composer.phar)
-    else
-    	pushd $vz_dir
-	    php -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));"
-	    COMPOSER="$vz_dir/composer.phar"
-    	popd
-    fi
+	pushd "$vz_dir"
+		echo "composer not found, downloading..."
+		php -r "eval('?>'.file_get_contents('https://getcomposer.org/installer'));"
+		COMPOSER="$vz_dir/composer.phar"
+	popd
 fi
 
 ###############################
 echo
 echo "install dependencies..."
 
-pushd $vz_dir
-$COMPOSER install --no-dev
+pushd "$vz_dir"
+"$COMPOSER" install --no-dev
 popd
 
 ###############################
 echo
 ask "configure volkszaehler.org?" y
 
-config=$vz_dir/etc/volkszaehler.conf.php
+config="$vz_dir/etc/volkszaehler.conf.php"
 
 if [ "$REPLY" == "y" ]; then
 	# test for pdo_mysql php module
@@ -172,20 +172,20 @@ if [ "$REPLY" == "y" ]; then
 	fi
 	
 	ask "mysql user?" vz
-	db_user=$REPLY
+	db_user="$REPLY"
 	ask "mysql password?" demo
-	db_pass=$REPLY
+	db_pass="$REPLY"
 	get_db_name
 
 	# we are using "|" as delimiter for sed to avoid escaped sequences in $dt_dir
-	sed -e "s|^\(\$config\['db'\]\['user'\]\).*|\1 = '$db_user';|" \
+	sed	-e "s|^\(\$config\['db'\]\['user'\]\).*|\1 = '$db_user';|" \
 		-e "s|^\/*\(\$config\['db'\]\['password'\]\).*|\1 = '$db_pass';|" \
 		-e "s|^\/*\(\$config\['db'\]\['dbname'\]\).*|\1 = '$db_name';|" \
 		-e "s|^\/*\(\$config\['lib'\]\['doctrine'\]\).*|\1 = '$dt_dir';|" \
-	< $vz_dir/etc/volkszaehler.conf.template.php \
-	> $config
+	< "$vz_dir/etc/volkszaehler.conf.template.php" \
+	> "$config"
 	
-	pushd $vz_dir
+	pushd "$vz_dir"
 	php misc/tools/doctrine orm:generate-proxies
 	popd
 fi
@@ -197,13 +197,13 @@ if [ "$REPLY" == "y" ]; then
 	get_admin
 
 	echo "creating database $db_name..."
-	mysql -h$db_host -u$db_admin_user -p$db_admin_pass -e 'CREATE DATABASE `'$db_name'`'
-	pushd $vz_dir
+	mysql -h"$db_host" -u"$db_admin_user" -p"$db_admin_pass" -e 'CREATE DATABASE `'"$db_name'`'
+	pushd "$vz_dir"
 	php misc/tools/doctrine orm:schema-tool:create
 	popd
 
 	echo "creating db user $db_user with proper rights..."
-	mysql -h$db_host -u$db_admin_user -p$db_admin_pass <<-EOF
+	mysql -h"$db_host" -u"$db_admin_user" -p"$db_admin_pass" <<-EOF
 		CREATE USER '$db_user'@'$db_host' IDENTIFIED BY '$db_pass';
 		GRANT USAGE ON *.* TO '$db_user'@'$db_host';
 		GRANT SELECT, UPDATE, INSERT ON $db_name.* TO '$db_user'@'$db_host';
@@ -217,8 +217,8 @@ ask "insert demo data in to database?" n
 if [ "$REPLY" == "y" ]; then
 	get_admin
 	get_db_name
-	cat $vz_dir/misc/sql/demo/entities.sql $vz_dir/misc/sql/demo/properties.sql $vz_dir/misc/sql/demo/data-demoset1.sql |
-	mysql -h$db_host -u$db_admin_user -p$db_admin_pass $db_name
+	cat "$vz_dir/misc/sql/demo/entities.sql" "$vz_dir/misc/sql/demo/properties.sql" "$vz_dir/misc/sql/demo/data-demoset1.sql" |
+		mysql -h"$db_host" -u"$db_admin_user" -p"$db_admin_pass" "$db_name"
 fi
 
 cleanup
