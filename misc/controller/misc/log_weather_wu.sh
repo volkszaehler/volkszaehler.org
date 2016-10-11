1ae2bc4d12f010d055a1855c4994386532cc8f4c
schnello <schnello@users.noreply.github.com>
2016-10-11 10:16:15 +0200

    Update log_weather_wu.sh
    Some improments

--- misc/controller/misc/log_weather_wu.sh
+++ misc/controller/misc/log_weather_wu.sh
@@ -1,6 +1,7 @@
+
 #!/bin/bash
 ## @outhor Norbert Walter <norbert-walter@web.de>
-## Cron: 39  *	* * *		~/log_weather_wu.sh >> log.weather_wu
+## Cron: 39  *  * * *           ~/log_weather_wu.sh >> log.weather_wu
 ## This is a service from www.wunderground.com
 ## Please install on Ubuntu the package curl before using the script
 ## This script get accurate weather data directly from personal weather stations
@@ -15,11 +16,18 @@
 ## eg. APPID='1234567890123456'
 APPID=''
 
+if [ ! -n "$APPID" ]; then
+        echo "APPID is empty! Please set the Application Key"
+        exit 1
+fi
+
+
 ## Personal weater station location name
 ## The station name you find on the webpage www.wunderground.com
 ## Notice: Using only the personal weather station (PWS) name. Other names are not
 ## working!
-PWS='ISACHSEN356'
+PWS='IVCKLABR5'
+
 
 ## Selection time source for saving in database
 ## 0 = using local PC time
@@ -27,7 +35,7 @@ PWS='ISACHSEN356'
 TS='1'
 
 ## middleware url
-URL="http://127.0.0.1/middleware"
+URL="http://127.0.0.1:80/middleware.php"
 
 ## UUIDs of the sensor in the volkszaehler database
 ## If UUID is empty, data won't be persisted.
@@ -43,102 +51,83 @@ UUID_uvindex=''
 
 #### Dont edit the following lines! ####
 
-## WeatherUnderground url
-## http://api.wunderground.com/api/1b0d33c840c54ac4/conditions/q/pws:ISACHSEN356.json
-WU="http://api.wunderground.com/api/$APPID/conditions/q/pws:$PWS.json"
-
-## paths to binaries - you should not need to change these
-CURL=/usr/bin/curl
-NC=/bin/nc
-
-weatherfile="/tmp/wunderground.$PWS"
 ## Get data from OpenWeatherMap
-$CURL --data "" "$WU" > "$weatherfile"
-test -s "$weatherfile" || exit 1 ## file is empty
+weatherfile="$(curl --data "" -s "http://api.wunderground.com/api/$APPID/conditions/q/pws:$PWS.json")"
+
+
+if [ $(grep -c "keynotfound" <<< "$weatherfile" ) -ge "1" ];then
+        echo "wrong API key"
+        exit 1
+fi
+
+
 ## Grep the weather data from answare
 ## $ echo "foo start blah blah blah stop bar" | sed 's/.*start \(.*\) stop.*/\1/'
 ## result: blah blah blah
-last_update="`grep 'observation_epoch":"' "$weatherfile"|sed -e 's/.*":"\(.*\)".*/\1/'`"
-temperature="`grep '"temp_c":' "$weatherfile"|sed -e 's/.*c":\(.*\),.*/\1/'`"
-pressure="`grep '"pressure_mb":"' "$weatherfile"|sed -e 's/.*mb":"\(.*\)".*/\1/'`"
-relative_humidity="`grep '"relative_humidity":"' "$weatherfile"|sed -e 's/.*":"\(.*\)%.*/\1/'`"
-dewpoint="`grep '"dewpoint_c":' "$weatherfile"|sed -e 's/.*c":\(.*\),.*/\1/'`"
-winddirection="`grep '"wind_degrees":' "$weatherfile"|sed -e 's/.*s":\(.*\),.*/\1/'`"
-wind="`grep '"wind_kph":' "$weatherfile"|sed -e 's/.*kph":\(.*\),.*/\1/'`"
-solar="`grep '"solarradiation":"' "$weatherfile"|sed -e 's/.*on":"\(.*\)".*/\1/'`"
-uvindex="`grep '"UV":"' "$weatherfile"|sed -e 's/.*UV":"\(.*\)",".*/\1/'`"
+last_update="`(grep "observation_epoch" <<< "$weatherfile" | sed -e 's/.*":"\(.*\)".*/\1/')`"
+temperature="`grep '"temp_c":' <<< "$weatherfile"|sed -e 's/.*c":\(.*\),.*/\1/'`"
+pressure="`grep '"pressure_mb"' <<< "$weatherfile"|sed -e 's/.*mb":"\(.*\)".*/\1/'`"
+relative_humidity="`grep '"relative_humidity"' <<< "$weatherfile"|sed -e 's/.*":"\(.*\)%.*/\1/'`"
+dewpoint="`grep '"dewpoint_c":' <<< "$weatherfile"|sed -e 's/.*c":\(.*\),.*/\1/'`"
+winddirection="`grep '"wind_degrees":' <<< "$weatherfile"|sed -e 's/.*s":\(.*\),.*/\1/'`"
+wind="`grep '"wind_kph":' <<< "$weatherfile"|sed -e 's/.*kph":\(.*\),.*/\1/'`"
+solar="`grep '"solarradiation"' <<< "$weatherfile"|sed -e 's/.*on":"\(.*\)".*/\1/'`"
+uvindex="`grep '"UV"' <<< "$weatherfile"|sed -e 's/.*UV":"\(.*\)",".*/\1/'`"
 
-## Convert the actual computer time in a timestamp
-dateA=`date +%Y-%m-%d`
-timeA=`date +%H:%M:%S`
 ## Timestamp actual computer time
 ## timestamp="`date -d "$timeA" "+%s000"`"
 ## Timestamp from OpenWeatherMap
-timestamp=$last_update"000"
+timestamp="$last_update"000
 
 ### Debug
 echo "Friendly weather service from WeatherUnderground (C)"
 echo "www.wunderground.com"
 echo -e "LocalTime: "`date`
 echo -e "UTC Time: "`date -u`
-echo "PWS: "$PWS
-echo "Temp: "$temperature"°C"
-echo "Humi: "$relative_humidity"%"
-echo "Pres: "$pressure"hPa"
-echo "Dewp: "$dewpoint"°C"
-echo "WDir: "$winddirection"°"
-echo "Wind: "$wind"km/h"
-echo "Solar: "$solar"W/m2"
-echo "UV: "$uvindex""
-echo "Date: "$dateA $timeA
-echo "Last Update: "$last_update
-echo "Timestamp: "$timestamp
+echo "PWS: $PWS"
+echo "Temp: $temperature°C"
+echo "Humi: $relative_humidity%"
+echo "Pres: $pressure hPa"
+echo "Dewp: $dewpoint°C"
+echo "WDir: $winddirection°"
+echo "Wind: $wind km/h"
+echo "Solar: $solarW/m2"
+echo "UV: $uvindex"
+echo "Date: $(date +%Y-%m-%d\ %H:%M:%S)"
+echo "Last Update: $last_update"
+echo "Timestamp: $timestamp"
 ### Debug
 
 
 # Save the data with or withou timestamp from WeatherUnderground
 # when exist a UUID
-if [ -n "$APPID" ]; then
-  if [ $TS = "0" ]; then
-    echo "Saving all values with local PC time"
-    timestring=""
-  else
-    echo "Saving all values with update time from WeatherUnderground"
-    timestring="ts="$timestamp"&"
-  fi
 
-  if [ -n "$UUID_temperature" ]; then
-    echo "Save Temp with UUID: "$UUID_temperature" in database"
-    $CURL --data "" "$URL/data/$UUID_temperature.json?$timestring""value=$temperature" &>/dev/null
-  fi
-  if [ -n "$UUID_pressure" ]; then
-    echo "Save Pressure with UUID: "$UUID_pressure" in database"
-    $CURL --data "" "$URL/data/$UUID_pressure.json?$timestring""value=$pressure" &>/dev/null
-  fi
-  if [ -n "$UUID_relative_humidity" ]; then
-    echo "Save Humidity  with UUID: "$UUID_relative_humidity" in database"
-    $CURL --data "" "$URL/data/$UUID_relative_humidity.json?$timestring""value=$relative_humidity" &>/dev/null
-  fi
-  if [ -n "$UUID_dewpoint" ]; then
-    echo "Save Dewpoint with UUID: "$UUID_dewpoint" in database"
-    $CURL --data "" "$URL/data/$UUID_dewpoint.json?$timestring""value=$dewpoint" &>/dev/null
-  fi
-  if [ -n "$UUID_winddirection" ]; then
-    echo "Save Winddirection with UUID: "$UUID_winddirection" in database"
-    $CURL --data "" "$URL/data/$UUID_winddirection.json?$timestring""value=$winddirection" &>/dev/null
-  fi
-  if [ -n "$UUID_wind" ]; then
-    echo "Save Windspeed with UUID: "$UUID_wind" in database"
-    $CURL --data "" "$URL/data/$UUID_wind.json?$timestring""value=$wind" &>/dev/null
-  fi
-  if [ -n "$UUID_solar" ]; then
-    echo "Save Solar with UUID: "$UUID_solar" in database"
-    $CURL --data "" "$URL/data/$UUID_solar.json?$timestring""value=$solar" &>/dev/null
-  fi
-  if [ -n "$UUID_uvindex" ]; then
-    echo "Save UV-Index with UUID: "$UUID_uvindex" in database"
-    $CURL --data "" "$URL/data/$UUID_uvindex.json?$timestring""value=$uvindex" &>/dev/null
-  fi
+if [ "$TS" = "0" ]; then
+        echo "Saving all values with local PC time"
+        timestring=""
 else
-  echo "APPID is empty! Please set the Application Key"
+        echo "Saving all values with update time from WeatherUnderground"
+        timestring="$timestamp"
 fi
+
+
+push (){
+
+curl --data "" -s "$URL/data/$1.json?ts=$2&value=$3"
+        }
+
+
+
+for i in "temperature" "pressure" "relative_humidity" "dewpoint" "winddirection" "wind" "solar" "uvindex";do
+        UUID="$(eval echo \$UUID_$i)"
+        value="$(eval echo \$$i)"
+
+        if [ -n "$UUID" ];then
+                echo "Save $i with UUID: $UUID in database"
+                push "$UUID" "$timestring" "$value"
+        fi
+
+done
+
+
+exit
