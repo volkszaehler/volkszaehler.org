f0f5759cf55f6c0c3b76635376a370df2299f618
andig <cpuidle@gmx.de>
2017-01-04 09:05:31 +0100

    Prevent additional errors when login required

--- htdocs/js/entities.js
+++ htdocs/js/entities.js
@@ -72,15 +72,14 @@ vz.entities.loadDetails = function() {
 		// Use thenable form and skip default error handling to allow modifying deferred resolution for handling
 		// invalid/deleted entity uuids. Otherwise frontend loading will stall.
 		queue.push(entity.loadDetails(true).then(
-			function(json) {
-				return $.Deferred().resolveWith(this, [json]);
-			},
+			null,	// success - no changes needed
 			function(xhr) {
 				var exception = (xhr.responseJSON || {}).exception;
 				// default error handling is skipped - be careful
 				if (exception && exception.message.match(/^Invalid UUID|^No entity found with UUID/)) {
 					vz.entities.splice(vz.entities.indexOf(entity), 1); // remove
-					return xhr;
+					// return new resolved deferred
+					$.Deferred().resolveWith(this, [xhr]);
 				}
 				return vz.load.errorHandler(xhr);
 			}
