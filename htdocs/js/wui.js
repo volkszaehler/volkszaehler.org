2a63f00fb96c14a68b0fbd160ec006be5ce6aa4f
andig <cpuidle@gmx.de>
2017-01-13 18:18:48 +0100

    Make unauthorized requests retryable

--- htdocs/js/wui.js
+++ htdocs/js/wui.js
@@ -1001,7 +1001,8 @@ vz.wui.dialogs.middlewareException = function(xhr) {
 vz.wui.dialogs.authorizationException = function(xhr) {
 	// suppress further errors
 	vz.wui.errorDialog = true;
-	var middleware = xhr.middleware;
+	var middleware = xhr.middleware,
+			deferred = $.Deferred();
 
 	$('#authorization').dialog({
 		title: unescape('Login'),
@@ -1040,7 +1041,7 @@ vz.wui.dialogs.authorizationException = function(xhr) {
 							var mw = vz.middleware.find(middleware);
 							if (mw) {
 								mw.setAuthorization(json.authtoken);
-								window.location.reload(false);	// reload
+								deferred.resolve(json.authtoken);
 							}
 						}
 					},
@@ -1058,4 +1059,6 @@ vz.wui.dialogs.authorizationException = function(xhr) {
 		}
 	})
 	.dialog('open');
+
+	return deferred;
 };
