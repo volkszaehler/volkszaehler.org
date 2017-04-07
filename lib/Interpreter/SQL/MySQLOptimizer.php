aeb028949a9d4f37695f6214e0edea6ee493fa10
andig <cpuidle@gmx.de>
2017-04-07 16:34:08 +0200

    Simplified weighed average implementation

--- lib/Interpreter/SQL/MySQLOptimizer.php
+++ lib/Interpreter/SQL/MySQLOptimizer.php
@@ -100,34 +100,37 @@ class MySQLOptimizer extends SQLOptimizer {
 		if (get_class($this->interpreter) !== Interpreter\SensorInterpreter::class)
 			return parent::optimizeDataSQL($sql, $sqlParameters);
 
-		// SensorInterpreter needs weighed average calculation.
+		// SensorInterpreter needs weighed average calculation
+		$foo = array();
+		$sqlTimeFilter = $this->interpreter->buildDateTimeFilterSQL($this->from, $this->to, $foo);
+
+		// NOTE		GREATEST() is required to force MySQL to evaluate the variables in the needed order (hacky)
+		// see:		https://bugs.php.net/bug.php?id=67537
+		// 			http://stackoverflow.com/questions/24457442/how-to-find-previous-record-n-per-group-maxtimestamp-timestamp
+		// 			http://www.xaprb.com/blog/2006/12/15/advanced-mysql-user-variable-techniques/
+		$sensorSql =
+			'SELECT MAX(agg.timestamp) AS timestamp, ' .
+				  'COALESCE( ' .
+					  'SUM(agg.val_by_time) / (MAX(agg.timestamp) - MIN(agg.prev_timestamp)), ' .
+					  $this->interpreter->groupExprSQL('agg.value') .
+				  ') AS value, ' .
+				  'COUNT(agg.value) AS count ' .
+		   'FROM ( ' .
+				'SELECT timestamp, value, ' .
+					'value * (timestamp - @prev_timestamp) AS val_by_time, ' .
+					'COALESCE(@prev_timestamp, 0) AS prev_timestamp, ' .
+					'@prev_timestamp := timestamp ' .
+				'FROM data ' .
+				'CROSS JOIN (SELECT @prev_timestamp := NULL) AS vars ' .
+				'WHERE channel_id=? ' . $sqlTimeFilter . ' ' .
+				'ORDER BY timestamp ' .
+		   ') AS agg ';
+
 		// MySQL-specific implementation below
 		if ($this->groupBy) {
-			$foo = array();
-			$sqlTimeFilter = $this->interpreter->buildDateTimeFilterSQL($this->from, $this->to, $foo);
 			$sqlGroupFields = $this->interpreter->buildGroupBySQL($this->groupBy);
 
-			// SensorInterpreter needs weighed average
-			// note:	GREATEST() is required to force MySQL to evaluate the variables in the needed order (hacky)
-			// see:		https://bugs.php.net/bug.php?id=67537
-			// 			http://stackoverflow.com/questions/24457442/how-to-find-previous-record-n-per-group-maxtimestamp-timestamp
-			// 			http://www.xaprb.com/blog/2006/12/15/advanced-mysql-user-variable-techniques/
-			$sql = 'SELECT MAX(agg.timestamp) AS timestamp, ' .
-						  'COALESCE( ' .
-							  'SUM(agg.val_by_time) / (MAX(agg.timestamp) - MIN(agg.prev_timestamp)), ' .
-							  $this->interpreter->groupExprSQL('agg.value') .
-						  ') AS value, ' .
-						  'COUNT(agg.value) AS count ' .
-				   'FROM ( ' .
-						'SELECT timestamp, value, ' .
-							'value * (timestamp - @prev_timestamp) AS val_by_time, ' .
-							'GREATEST(0, IF(@prev_timestamp = NULL, NULL, @prev_timestamp)) AS prev_timestamp, ' .
-							'@prev_timestamp := timestamp ' .
-						'FROM data ' .
-						'CROSS JOIN (SELECT @prev_timestamp := NULL) AS vars ' .
-						'WHERE channel_id=? ' . $sqlTimeFilter . ' ' .
-						'ORDER BY timestamp ' .
-				   ') AS agg ' .
+			$sql = $sensorSql .
 				   'GROUP BY ' . $sqlGroupFields . ' ' .
 				   'ORDER BY timestamp ASC';
 			return true;
@@ -144,34 +147,14 @@ class MySQLOptimizer extends SQLOptimizer {
 				$timestampOffset = $this->from - $packageSize + 1;
 
 				// optimize package statement general case: tuple packaging
-				$foo = array();
-				$sqlTimeFilter = $this->interpreter->buildDateTimeFilterSQL($this->from, $this->to, $foo);
-				// $this->rowCount = floor($this->rowCount / $packageSize);
-
-				// Speedup - SensorInterpreter case (weighed average calculation)
-				$sql = 'SELECT MAX(agg.timestamp) AS timestamp, ' .
-							  'COALESCE( ' .
-								  'SUM(agg.val_by_time) / (MAX(agg.timestamp) - MIN(agg.prev_timestamp)), ' .
-								  $this->interpreter->groupExprSQL('agg.value') .
-							  ') AS value, ' .
-							  'COUNT(agg.value) AS count ' .
-					   'FROM ( ' .
-							'SELECT timestamp, value, ' .
-								'value * (timestamp - @prev_timestamp) AS val_by_time, ' .
-								'GREATEST(0, IF(@prev_timestamp = NULL, NULL, @prev_timestamp)) AS prev_timestamp, ' .
-								'@prev_timestamp := timestamp ' .
-							'FROM data ' .
-							'CROSS JOIN (SELECT @prev_timestamp := NULL) AS vars ' .
-							'WHERE channel_id=? ' . $sqlTimeFilter . ' ' .
-							'ORDER BY timestamp ASC' .
-					   ') AS agg ' .
+				$sql = $sensorSql .
 					   'GROUP BY (timestamp - ' . $timestampOffset . ') >> ' . $bitShift . ' ' .
 					   'ORDER BY timestamp ASC';
-
 				return true;
 			}
 		}
 
+		// we know the parent optimizer doesn't habe anything better to offer
 		return false;
 	}
 }
