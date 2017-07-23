b770da61561a4fd0c0648bbd08e52cd8b53626a1
andig <cpuidle@gmx.de>
2017-07-23 13:11:28 +0200

    Avoid hard failure

--- lib/Server/PPMBootstrapAdapter.php
+++ lib/Server/PPMBootstrapAdapter.php
@@ -29,6 +29,10 @@ use PHPPM\Bootstraps\BootstrapInterface;
 
 // move out of lib/server
 define('VZ_DIR', realpath(__DIR__ . '/../..'));
+// check config file before bootstrap dies
+if (!file_exists(VZ_DIR . '/etc/volkszaehler.conf.php')) {
+	throw new \Exception('Could not find config file.');
+}
 require_once(VZ_DIR . '/lib/bootstrap.php');
 
 /**
