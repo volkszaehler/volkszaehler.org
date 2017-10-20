520453e3d5b19b12eb2d2dfec5d82d88d527e9c2
andig <cpuidle@gmx.de>
2017-10-20 20:28:18 +0200

    Fix authentication via CORS

--- lib/View/JSON.php
+++ lib/View/JSON.php
@@ -66,6 +66,7 @@ class JSON extends View {
 		$this->response->headers->set('Content-Type', 'application/json');
 		$this->response->headers->set('Access-Control-Allow-Origin', '*');
 		$this->response->headers->set('Access-Control-Allow-Headers', 'Authorization');
+		$this->response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS, DELETE');
 	}
 
 	/**
