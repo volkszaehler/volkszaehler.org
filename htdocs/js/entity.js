6189d121315a2c9fb98ee83d3bd7fafecf00dbec
Christian Weiske <cweiske@cweiske.de>
2021-10-20 22:16:22 +0200

    Display line breaks in channel description
    The description of a channel is a string and may contain newlines.
    This patch replaces them with <br> tags so that they are properly
    visible when viewing the channel details.
    
    (Description is hidden by default and needs to be enabled in
    htdocs/js/options.js, hiddenProperties)
    
    Resolves: https://github.com/volkszaehler/volkszaehler.org/issues/713

--- htdocs/js/entity.js
+++ htdocs/js/entity.js
@@ -623,6 +623,10 @@ Entity.prototype.getDOMDetails = function (edit) {
 						value = Number(value * 100).toFixed(2) + prefix + vz.wui.formatConsumptionUnit(this.getUnit());
 						break;
 
+					case 'description':
+						value = (value + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1<br/>$2');
+						break;
+
 					case 'resolution':
 						prefix = (this.getUnit() && this.definition.scale == 1000) ? 'k' : ''; // per Wh or kWh
 						value += '/' + prefix + vz.wui.formatConsumptionUnit(this.getUnit());
